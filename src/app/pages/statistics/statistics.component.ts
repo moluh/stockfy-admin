5b9049aa5615d18bb5da31483c8ad576e1cac49e
Lucas Moretti <morettilucash@gmail.com>
2021-07-10 16:06:00 -0300

    Agregado grafico de lineas

--- src/app/pages/statistics/statistics.component.ts
+++ src/app/pages/statistics/statistics.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
-import { Label } from 'ng2-charts';
+import { Color, Label } from 'ng2-charts';
 import * as dayjs from 'dayjs';
 import * as utc from 'dayjs/plugin/utc';
 import { StatisticsService } from 'src/app/services/statistics.service';
 dayjs.extend(utc);
+import { ArraysGraphics } from './arraysGraphics';
 
 @Component({
   selector: 'app-statistics',
@@ -15,11 +16,44 @@ export class StatisticsComponent implements OnInit {
   title: string = 'Estadísticas';
   statisticsDashboard: any = null;
   statisticsGraphic: any = null;
+  styleGraphic: string = 'bar';
   fromDashboard: any = dayjs(new Date()).format('YYYY-MM-DD');
   toDashboard: any = dayjs(new Date()).add(1, 'month').format('YYYY-MM-DD');
   fromGraphic: any = dayjs(new Date()).format('YYYY-MM-DD');
   toGraphic: any = dayjs(new Date()).add(1, 'month').format('YYYY-MM-DD');
 
+  // ================================
+  // BAR STYLE GRAPHIC
+  // ================================
+  public lineChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,0,0,0.3)',
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+  public lineChartPlugins = [];
+
+  // LABELS
+  lineLabelsCountMovimientos: Label[] = [];
+  lineLabelsCountPagos: Label[] = [];
+  lineLabelsGanancias: Label[] = [];
+  lineLabelsTotalDePagos: Label[] = [];
+  lineLabelsTotalVendido: Label[] = [];
+  // DATA
+  lineDataCountMovimientos: ChartDataSets[] = [];
+  lineDataCountPagos: ChartDataSets[] = [];
+  lineDataGanancias: ChartDataSets[] = [];
+  lineDataTotalDePagos: ChartDataSets[] = [];
+  lineDataTotalVendido: ChartDataSets[] = [];
+
+  // ================================
+  // BAR STYLE GRAPHIC
+  // ================================
   barChartOptions: ChartOptions = {
     responsive: true,
   };
@@ -27,52 +61,17 @@ export class StatisticsComponent implements OnInit {
   barChartLegend = true;
   barChartPlugins = [];
   // LABELS
-  labelsCountMovimientos: Label[] = [];
-  labelsCountPagos: Label[] = [];
-  labelsGanancias: Label[] = [];
-  labelsTotalDePagos: Label[] = [];
-  labelsTotalVendido: Label[] = [];
+  barLabelsCountMovimientos: Label[] = [];
+  barLabelsCountPagos: Label[] = [];
+  barLabelsGanancias: Label[] = [];
+  barLabelsTotalDePagos: Label[] = [];
+  barLabelsTotalVendido: Label[] = [];
   // DATA
-  dataCountMovimientos: ChartDataSets[] = [
-    {
-      data: [],
-      label: 'Cantidad de ventas',
-      backgroundColor: this.randomHexColor(),
-      hoverBackgroundColor: this.randomHexColor(),
-    },
-  ];
-  dataCountPagos: ChartDataSets[] = [
-    {
-      data: [],
-      label: 'Cantidad de ventas',
-      backgroundColor: this.randomHexColor(),
-      hoverBackgroundColor: this.randomHexColor(),
-    },
-  ];
-  dataGanancias: ChartDataSets[] = [
-    {
-      data: [],
-      label: 'Ganancias por intereses',
-      backgroundColor: this.randomHexColor(),
-      hoverBackgroundColor: this.randomHexColor(),
-    },
-  ];
-  dataTotalDePagos: ChartDataSets[] = [
-    {
-      data: [],
-      label: 'Monto total de pagos',
-      backgroundColor: this.randomHexColor(),
-      hoverBackgroundColor: this.randomHexColor(),
-    },
-  ];
-  dataTotalVendido: ChartDataSets[] = [
-    {
-      data: [],
-      label: 'Monto total vendido',
-      backgroundColor: this.randomHexColor(),
-      hoverBackgroundColor: this.randomHexColor(),
-    },
-  ];
+  barDataCountMovimientos: ChartDataSets[] = [];
+  barDataCountPagos: ChartDataSets[] = [];
+  barDataGanancias: ChartDataSets[] = [];
+  barDataTotalDePagos: ChartDataSets[] = [];
+  barDataTotalVendido: ChartDataSets[] = [];
 
   constructor(private _stats: StatisticsService) {}
 
@@ -81,8 +80,12 @@ export class StatisticsComponent implements OnInit {
     // npm un ng2-charts chart.js
   }
 
+  changeStyleGraphic() {
+    this.styleGraphic = this.styleGraphic === 'bar' ? 'line' : 'bar';
+  }
+
   randomHexColor() {
-    const random = '#' + Math.floor(Math.random() * 16777215).toString(16)
+    const random = '#' + Math.floor(Math.random() * 16777215).toString(16);
     return random;
   }
 
@@ -95,9 +98,9 @@ export class StatisticsComponent implements OnInit {
   }
 
   getStatsGraphics() {
-    this.cleanData()
+    this.resetData();
     this._stats
-      .getBetweenDatesGraphic(this.fromDashboard, this.toDashboard)
+      .getBetweenDatesGraphic(this.fromGraphic, this.toGraphic)
       .subscribe((data) => {
         this.setGraphicData(data);
       });
@@ -105,69 +108,133 @@ export class StatisticsComponent implements OnInit {
 
   setGraphicData(data) {
     data.map((stat) => {
-      // labels
-      this.labelsCountMovimientos.push(dayjs(stat.Fecha).format('DD-MM-YYYY'));
-      this.labelsCountPagos.push(dayjs(stat.Fecha).format('DD-MM-YYYY'));
-      this.labelsGanancias.push(dayjs(stat.Fecha).format('DD-MM-YYYY'));
-      this.labelsTotalDePagos.push(dayjs(stat.Fecha).format('DD-MM-YYYY'));
-      this.labelsTotalVendido.push(dayjs(stat.Fecha).format('DD-MM-YYYY'));
-      // datos
-      this.dataCountMovimientos[0].data.push(parseInt(stat.CountMovimientos));
-      this.dataCountPagos[0].data.push(parseInt(stat.CountPagos));
-      this.dataGanancias[0].data.push(stat.Ganancias);
-      this.dataTotalDePagos[0].data.push(stat.TotalDePagos);
-      this.dataTotalVendido[0].data.push(stat.TotalVendido);
+      // line labels
+      this.lineLabelsCountMovimientos.push(dayjs(stat.Fecha).format('DD-MMM'));
+      this.lineLabelsCountPagos.push(dayjs(stat.Fecha).format('DD-MMM'));
+      this.lineLabelsGanancias.push(dayjs(stat.Fecha).format('DD-MMM'));
+      this.lineLabelsTotalDePagos.push(dayjs(stat.Fecha).format('DD-MMM'));
+      this.lineLabelsTotalVendido.push(dayjs(stat.Fecha).format('DD-MMM'));
+      // line datos
+      this.lineDataCountMovimientos[0].data.push(
+        parseInt(stat.CountMovimientos)
+      );
+      this.lineDataCountPagos[0].data.push(parseInt(stat.CountPagos));
+      this.lineDataGanancias[0].data.push(stat.Ganancias);
+      this.lineDataTotalDePagos[0].data.push(stat.TotalDePagos);
+      this.lineDataTotalVendido[0].data.push(stat.TotalVendido);
+
+      // bar labels
+      this.barLabelsCountMovimientos.push(dayjs(stat.Fecha).format('DD-MMM'));
+      this.barLabelsCountPagos.push(dayjs(stat.Fecha).format('DD-MMM'));
+      this.barLabelsGanancias.push(dayjs(stat.Fecha).format('DD-MMM'));
+      this.barLabelsTotalDePagos.push(dayjs(stat.Fecha).format('DD-MMM'));
+      this.barLabelsTotalVendido.push(dayjs(stat.Fecha).format('DD-MMM'));
+      // bar datos
+      this.barDataCountMovimientos[0].data.push(
+        parseInt(stat.CountMovimientos)
+      );
+      this.barDataCountPagos[0].data.push(parseInt(stat.CountPagos));
+      this.barDataGanancias[0].data.push(stat.Ganancias);
+      this.barDataTotalDePagos[0].data.push(stat.TotalDePagos);
+      this.barDataTotalVendido[0].data.push(stat.TotalVendido);
     });
     this.statisticsGraphic = data;
   }
 
-  cleanData() {
-     // LABELS
-  this.labelsCountMovimientos = []
-  this.labelsCountPagos = []
-  this.labelsGanancias = []
-  this.labelsTotalDePagos = []
-  this.labelsTotalVendido = []
-  // DATA
-  this.dataCountMovimientos  = [
-    {
-      data: [],
-      label: 'Cantidad de ventas',
-      backgroundColor: this.randomHexColor(),
-      hoverBackgroundColor: this.randomHexColor(),
-    },
-  ];
-  this.dataCountPagos = [
-    {
-      data: [],
-      label: 'Cantidad de ventas',
-      backgroundColor: this.randomHexColor(),
-      hoverBackgroundColor: this.randomHexColor(),
-    },
-  ];
-  this.dataGanancias = [
-    {
-      data: [],
-      label: 'Ganancias por intereses',
-      backgroundColor: this.randomHexColor(),
-      hoverBackgroundColor: this.randomHexColor(),
-    },
-  ];
-  this.dataTotalDePagos = [
-    {
-      data: [],
-      label: 'Monto total de pagos',
-      backgroundColor: this.randomHexColor(),
-      hoverBackgroundColor: this.randomHexColor(),
-    },
-  ];
-  this.dataTotalVendido = [
-    {
-      data: [],
-      label: 'Monto total vendido',
-      backgroundColor: this.randomHexColor(),
-      hoverBackgroundColor: this.randomHexColor(),
-    },
-  ];
+  resetData() {
+    // LABELS
+    this.lineLabelsCountMovimientos = [];
+    this.lineLabelsCountPagos = [];
+    this.lineLabelsGanancias = [];
+    this.lineLabelsTotalDePagos = [];
+    this.lineLabelsTotalVendido = [];
+    // DATA
+    this.lineDataCountMovimientos = [
+      {
+        data: [],
+        label: 'Cantidad de ventas',
+        backgroundColor: this.randomHexColor(),
+        hoverBackgroundColor: this.randomHexColor(),
+      },
+    ];
+    this.lineDataCountPagos = [
+      {
+        data: [],
+        label: 'Cantidad de pagos',
+        backgroundColor: this.randomHexColor(),
+        hoverBackgroundColor: this.randomHexColor(),
+      },
+    ];
+    this.lineDataGanancias = [
+      {
+        data: [],
+        label: 'Ganancias por intereses',
+        backgroundColor: this.randomHexColor(),
+        hoverBackgroundColor: this.randomHexColor(),
+      },
+    ];
+    this.lineDataTotalDePagos = [
+      {
+        data: [],
+        label: 'Monto total de pagos',
+        backgroundColor: this.randomHexColor(),
+        hoverBackgroundColor: this.randomHexColor(),
+      },
+    ];
+    this.lineDataTotalVendido = [
+      {
+        data: [],
+        label: 'Monto total vendido',
+        backgroundColor: this.randomHexColor(),
+        hoverBackgroundColor: this.randomHexColor(),
+      },
+    ];
+    // LABELS
+    this.barLabelsCountMovimientos = [];
+    this.barLabelsCountPagos = [];
+    this.barLabelsGanancias = [];
+    this.barLabelsTotalDePagos = [];
+    this.barLabelsTotalVendido = [];
+    // DATA
+    this.barDataCountMovimientos = [
+      {
+        data: [],
+        label: 'Cantidad de ventas',
+        backgroundColor: this.randomHexColor(),
+        hoverBackgroundColor: this.randomHexColor(),
+      },
+    ];
+    this.barDataCountPagos = [
+      {
+        data: [],
+        label: 'Cantidad de ventas',
+        backgroundColor: this.randomHexColor(),
+        hoverBackgroundColor: this.randomHexColor(),
+      },
+    ];
+    this.barDataGanancias = [
+      {
+        data: [],
+        label: 'Ganancias por intereses',
+        backgroundColor: this.randomHexColor(),
+        hoverBackgroundColor: this.randomHexColor(),
+      },
+    ];
+    this.barDataTotalDePagos = [
+      {
+        data: [],
+        label: 'Monto total de pagos',
+        backgroundColor: this.randomHexColor(),
+        hoverBackgroundColor: this.randomHexColor(),
+      },
+    ];
+    this.barDataTotalVendido = [
+      {
+        data: [],
+        label: 'Monto total vendido',
+        backgroundColor: this.randomHexColor(),
+        hoverBackgroundColor: this.randomHexColor(),
+      },
+    ];
   }
 }
